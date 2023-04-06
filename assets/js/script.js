19f76dba1afeee807fbc807b0786c767393c7783
Jannieguo <124142282+Jannieguo@users.noreply.github.com>
2023-04-06 12:05:13 +0200

    12:05 06.04.23

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-// $(document).ready(function() {
-  window.onscroll = scrollPage;
+window.addEventListener('DOMContentLoaded', () => {
+  const main = document.querySelector('main')
+
+    // $(document).ready(function() {
+    // window.onscroll = scrollPage;
+    main.onscroll = scrollPage;
 
   const navbar = document.getElementById("navbar");
 
 
   function scrollPage() {
-    if ( window.pageYOffset >= 700 ) {
+    // if (window.pageYOffset >= 700) {
+      if (main.firstElementChild.getBoundingClientRect().top <= -700) {
       navbar.classList.remove("is-hidden")
     } else {
       navbar.classList.add("is-hidden");
@@ -13,57 +18,58 @@
     }
   }
 
-  const toggleMenu = ( ) => {
-    navbar.classList.toggle( 'closed' );
+  const toggleMenu = () => {
+    navbar.classList.toggle('closed');
   }
 
-  const menutoggle = document.querySelector( '#navbar a.toggle' );
-  menutoggle.addEventListener( 'click', toggleMenu );
-// } );
+  const menutoggle = document.querySelector('#navbar a.toggle');
+  menutoggle.addEventListener('click', toggleMenu);
+  // } );
 
 
-function showImage(imageId, event) {
-  event.preventDefault();
-  var image = document.getElementById(imageId);
-  if (image.style.display === "none") {
-    image.style.display = "block";
-  } else {
-    image.style.display = "none";
+  function showImage(imageId, event) {
+    event.preventDefault();
+    var image = document.getElementById(imageId);
+    if (image.style.display === "none") {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
   }
-}
 
-const imageModalLinks = document.querySelectorAll('.to-modal-image')
+  const imageModalLinks = document.querySelectorAll('.to-modal-image')
 
-imageModalLinks.forEach(function(imageModalLink) {
+  imageModalLinks.forEach(function (imageModalLink) {
 
-   
-    imageModalLink.addEventListener('click', function(e) {
 
-       
-        var imageModalLinkId = this.getAttribute('data-link-id')
-       
-        var imageModalTarget = document.querySelectorAll('[data-target-id="'+imageModalLinkId+'"]');
-        
-        imageModalTarget[0].classList.remove('is-hidden');
-       
-        e.preventDefault();
+    imageModalLink.addEventListener('click', function (e) {
+
+
+      var imageModalLinkId = this.getAttribute('data-link-id')
+
+      var imageModalTarget = document.querySelectorAll('[data-target-id="' + imageModalLinkId + '"]');
+
+      imageModalTarget[0].classList.remove('is-hidden');
+
+      e.preventDefault();
 
     })
 
+  })
+
+
+
+  const imageModals = document.querySelectorAll('.modal-image')
+
+  imageModals.forEach(function (imageModal) {
+
+
+    imageModal.addEventListener('click', function (e) {
+
+
+      this.classList.add('is-hidden');
+
+    })
+
+  });
 })
-
-
-
-const imageModals = document.querySelectorAll('.modal-image')
-
-imageModals.forEach(function(imageModal) {
-
-    
-    imageModal.addEventListener('click', function(e) {
-
-    
-        this.classList.add('is-hidden');
-
-    })
-
-});
